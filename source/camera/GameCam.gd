56cba06e9174f6e1759884c22fb6777322a40753
Aaron Winter <winter.aaron93@gmail.com>
2019-06-15 19:18:56 +0200

    screenshake

--- source/camera/GameCam.gd
+++ source/camera/GameCam.gd
@@ -1,10 +1,13 @@
 extends Camera2D
+class_name GameCam
 
 var _target : Character = null
 
 var fake_position := Vector2()
 
 export(NodePath) var target = null
+
+onready var screen_shake := $ScreenShake as ScreenShake
 
 func _ready() -> void:
 	_target = get_node(target)
--- source/game/Game.gd
+++ source/game/Game.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 onready var interface := $Interface as Interface
+onready var game_cam := $GameCam as GameCam
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		game_cam.screen_shake.start()
 
 func _on_Player_state_changed(state_name) -> void:
 	if interface:
