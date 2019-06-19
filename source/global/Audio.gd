766c782b04c00efc493ecefb260a4cc7e2510e14
Aaron Winter <winter.aaron93@gmail.com>
2019-06-19 23:00:02 +0200

    music at 0.7 percent volume

--- source/global/Audio.gd
+++ source/global/Audio.gd
@@ -1,13 +1,14 @@
 extends Node
 
-var master_volume : float setget _set_master_volume
+export var master_volume := 1.0 setget _set_master_volume
+export var music_volume := 1.0 setget _set_music_volume
+export var effects_volume := 1.0 setget _set_effects_volume
 
 onready var mixing_desk := $MixingDeskMusic
 
 func _ready() -> void:
 	mixing_desk._init_song("menuMusic")
 	mixing_desk._init_song("levelMusic")
-	_set_master_volume(0.7)
 
 func play_song(song: String) -> void:
 	mixing_desk._play(song)
@@ -15,3 +16,13 @@ func play_song(song: String) -> void:
 func _set_master_volume(value):
 	master_volume = value
 	AudioServer.set_bus_volume_db(0, linear2db(master_volume))
+
+
+func _set_music_volume(value):
+	music_volume = value
+	AudioServer.set_bus_volume_db(1, linear2db(music_volume))
+
+
+func _set_effects_volume(value):
+	effects_volume = value
+	AudioServer.set_bus_volume_db(2, linear2db(effects_volume))
