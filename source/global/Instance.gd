e8459bb303a6ff1113b9947227a37e50ea6a283c
Aaron Winter <winter.aaron93@gmail.com>
2019-06-19 21:02:54 +0200

    space button, lol

--- source/global/Instance.gd
+++ source/global/Instance.gd
@@ -3,9 +3,15 @@ extends Node
 const Dust = preload("res://source/particles/Dust.tscn")
 const Pulse = preload("res://source/particles/Pulse.tscn")
 const AfterImage = preload("res://source/particles/AfterImage.tscn")
+
 const Projectile = preload("res://source/projectiles/Projectile.tscn")
+
+const KeyButton = preload("res://source/interface/KeyButton.tscn")
 
 func Dust(): return Dust.instance()
 func Pulse(): return Pulse.instance()
 func AfterImage(): return AfterImage.instance()
+
 func Projectile(): return Projectile.instance()
+
+func KeyButton(): return KeyButton.instance()
---
+++ source/interface/hud/KeyPreview.gd
@@ -0,0 +1,26 @@
+extends Control
+
+var key = null
+
+onready var anim := $AnimationPlayer as AnimationPlayer
+
+func _ready() -> void:
+	new_key("SPACE")
+
+func new_key(text: String) -> void:
+	key = Instance.KeyButton()
+	key.text = text
+	add_child(key)
+	anim.play("fade_in")
+
+func clear() -> void:
+	get_child(0).queue_free()
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+
+	if anim_name == "fade_in":
+		anim.play("press")
+
+	if anim_name == "fade_out":
+		key.queue_free()
+		key = null
