e823beb38022b5e8147c60acd7aa55c676d9331d
Aaron Winter <winter.aaron93@gmail.com>
2019-06-21 23:07:45 +0200

    reset camera stuff

--- source/camera/GameCam.gd
+++ source/camera/GameCam.gd
@@ -4,19 +4,12 @@ class_name GameCam
 var _target = null
 var _prev_target = null
 
-var prev_position := Vector2()
-
 onready var timer := $Timer as Timer
 onready var screen_shake := $ScreenShake as ScreenShake
 
 func _process(delta: float) -> void:
 	if _target:
-		var direction = _target.global_position.x - prev_position.x
-		if direction > 5:
-			global_position = _target.global_position + Vector2(120, 0)
-		elif direction < -5:
-			global_position = _target.global_position + Vector2(-120, 0)
-		prev_position = _target.global_position
+		global_position = _target.global_position
 
 func change_target(new_target: Node2D, wait_time: float = 0) -> void:
 	if _target:
