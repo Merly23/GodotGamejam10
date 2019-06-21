331d983d30b1f808fa8c65bc72d8f5ce7b9babef
Aaron Winter <winter.aaron93@gmail.com>
2019-06-21 13:39:14 +0200

    made sure drone stays within seek distance

--- source/character/ai/drone/states/Idle.gd
+++ source/character/ai/drone/states/Idle.gd
@@ -6,7 +6,7 @@ export var max_speed := 160
 export var acceleration := 8
 
 onready var timer := $Timer as Timer
-
+onready var flip_timer := $FlipTimer as Timer
 func enter(host: Node) -> void:
 	host = host as Drone
 	host.play_lower("idle")
@@ -19,6 +19,10 @@ func update(host: Node, delta: float) -> void:
 		direction = -1
 	elif direction == -1 and host.terrain_on("left"):
 		direction = 1
+
+	if host.is_too_far_from_origin() and flip_timer.is_stopped():
+		direction *= -1
+		flip_timer.start()
 
 	if direction == -1:
 		host.motion.x = clamp(host.motion.x - acceleration, -max_speed, max_speed)
