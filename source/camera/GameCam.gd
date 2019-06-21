20750a47f1fe97b38f6a904afd1a8b870c936081
Aaron Winter <winter.aaron93@gmail.com>
2019-06-21 21:16:11 +0200

    camera offset to see better what's coming

--- source/camera/GameCam.gd
+++ source/camera/GameCam.gd
@@ -4,13 +4,19 @@ class_name GameCam
 var _target = null
 var _prev_target = null
 
+var prev_position := Vector2()
+
 onready var timer := $Timer as Timer
 onready var screen_shake := $ScreenShake as ScreenShake
 
 func _process(delta: float) -> void:
 	if _target:
-		global_position = _target.global_position
-
+		var direction = _target.global_position.x - prev_position.x
+		if direction > 0:
+			global_position = _target.global_position + Vector2(120, 0)
+		elif direction < 0:
+			global_position = _target.global_position + Vector2(-120, 0)
+		prev_position = _target.global_position
 func change_target(new_target: Node2D, wait_time: float = 0) -> void:
 	if _target:
 		_prev_target = _target
