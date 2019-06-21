d16d2e77d59753050df77d5bcf303378b0d2a87e
Aaron Winter <winter.aaron93@gmail.com>
2019-06-21 21:41:39 +0200

    godot wild jam splash

--- source/camera/GameCam.gd
+++ source/camera/GameCam.gd
@@ -12,11 +12,12 @@ onready var screen_shake := $ScreenShake as ScreenShake
 func _process(delta: float) -> void:
 	if _target:
 		var direction = _target.global_position.x - prev_position.x
-		if direction > 0:
+		if direction > 5:
 			global_position = _target.global_position + Vector2(120, 0)
-		elif direction < 0:
+		elif direction < -5:
 			global_position = _target.global_position + Vector2(-120, 0)
 		prev_position = _target.global_position
+
 func change_target(new_target: Node2D, wait_time: float = 0) -> void:
 	if _target:
 		_prev_target = _target
