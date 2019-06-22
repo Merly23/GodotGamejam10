5361aa9be1a5b19951b8f03217318297f11dda00
Aaron Winter <winter.aaron93@gmail.com>
2019-06-23 01:27:49 +0200

    images and checkpoint animations

--- source/checkpoint/Checkpoint.gd
+++ source/checkpoint/Checkpoint.gd
@@ -6,11 +6,14 @@ var active := false
 
 var id := 0
 
+onready var anim := $AnimationPlayer as AnimationPlayer
+
 func _ready() -> void:
 	id = get_index()
 
 func _on_Checkpoint_body_entered(body: PhysicsBody2D) -> void:
 	if body == Global.Player and not active:
 		active = true
+		anim.play("activate")
 		emit_signal("reached", id)
 		Audio.play_sfx("checkpoint")
