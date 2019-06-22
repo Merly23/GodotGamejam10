b7ed7c99408efdf380a83b803b8909bc87be0597
Aaron Winter <winter.aaron93@gmail.com>
2019-06-22 16:28:45 +0200

    glitcher now attack

--- source/character/ai/glitcher/Glitcher.gd
+++ source/character/ai/glitcher/Glitcher.gd
@@ -4,4 +4,10 @@ class_name Glitcher
 export var damage := 2
 
 func attack():
-	print("attack")
+	var bodies = hit_area.get_overlapping_bodies()
+
+	Audio.play_sfx("player_slash")
+
+	for body in bodies:
+		if body is Character and body.team_number != team_number:
+			body.hurt(damage)
