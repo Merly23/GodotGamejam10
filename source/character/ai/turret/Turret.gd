4e8bb73d176c44330279af00d1de9792bc80eea2
Aaron Winter <winter.aaron93@gmail.com>
2019-06-23 00:53:49 +0200

    turret

--- source/character/ai/turret/Turret.gd
+++ source/character/ai/turret/Turret.gd
@@ -8,7 +8,7 @@ export var max_shots := 2
 export var bullet_speed := 800
 export var bullet_damage := 1
 
-onready var barrel := $Barrel as Position2D
+onready var barrel := $Barrel
 
 func _ready() -> void:
 	fsm.change_state("idle")
@@ -22,18 +22,18 @@ func _register_states():
 
 func flip_left() -> void:
 	.flip_left()
-	barrel.position.x = -55
+	barrel.position.x = -14
 
 func flip_right() -> void:
 	.flip_right()
-	barrel.position.x = 55
+	barrel.position.x = 14
 
 func shoot() -> void:
 	var projectile = Instance.Projectile()
 	projectile.shooter = self
 	projectile.global_position = barrel.global_position
 	get_tree().current_scene.add_child(projectile)
-	projectile.fire(bullet_damage, bullet_speed, Vector2(get_player_direction(), 0))
+	projectile.fire(bullet_damage, bullet_speed, Vector2(1 if is_flipped() else -1, 0))
 
 func is_player_in_vision() -> bool:
 
@@ -41,10 +41,3 @@ func is_player_in_vision() -> bool:
 		return false
 
 	return global_position.distance_to(Global.Player.global_position) < vision
-
-func get_player_direction() -> int:
-
-	if not Global.Player:
-		return 1
-
-	return -1 if Global.Player.global_position.x < global_position.x else 1
--- source/character/ai/turret/states/Idle.gd
+++ source/character/ai/turret/states/Idle.gd
@@ -4,17 +4,10 @@ onready var timer := $Timer as Timer
 
 func enter(host: Node) -> void:
 	host = host as Character
-
 	timer.start()
 
 func update(host: Node, delta: float) -> void:
 	host = host as Character
 
 	if host.is_player_in_vision() and timer.is_stopped() and not host.disabled and host.can_move:
-
-		if host.get_player_direction() == -1:
-			host.flip_left()
-		else:
-			host.flip_right()
-
 		host.fsm.change_state("shoot")
