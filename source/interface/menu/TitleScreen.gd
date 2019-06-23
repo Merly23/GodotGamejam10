0a7069626aa78822273971611b8439236fa52c09
Aaron Winter <winter.aaron93@gmail.com>
2019-06-23 04:06:22 +0200

    no load button

--- source/interface/menu/TitleScreen.gd
+++ source/interface/menu/TitleScreen.gd
@@ -4,8 +4,8 @@ func _ready() -> void:
 	Audio.play_music("menu_music")
 	Glitch.level = 1
 
-	if not Global.current_level:
-		$CenterContainer/VBoxContainer/Buttons/Load.visible = false
+#	if not Global.current_level:
+#		$CenterContainer/VBoxContainer/Buttons/Load.visible = false
 
 func _on_Play_pressed() -> void:
 	Scene.change(Scene.Level1)
