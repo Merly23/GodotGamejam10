f1f581d2b2ea733fc6f10b6680a94d179ddbc591
Aaron Winter <winter.aaron93@gmail.com>
2019-06-28 13:02:09 +0200

    minor cleanup

--- source/interface/menu/TitleScreen.gd
+++ source/interface/menu/TitleScreen.gd
@@ -4,9 +4,6 @@ func _ready() -> void:
 	Audio.play_music("menu_music")
 	Glitch.level = 1
 
-#	if not Global.current_level:
-#		$CenterContainer/VBoxContainer/Buttons/Load.visible = false
-
 func _on_Play_pressed() -> void:
 	Scene.change(Scene.Level1)
 
@@ -18,7 +15,6 @@ func _on_Quit_pressed() -> void:
 
 func _on_Load_pressed() -> void:
 	Scene.change(Global.current_level)
-
 
 func _on_Settings_pressed() -> void:
 	Scene.change(Scene.Settings)
