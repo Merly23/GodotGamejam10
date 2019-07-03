1e7c610f33e8e4c8663ffadd80922491c3874d33
Aaron Winter <winter.aaron93@gmail.com>
2019-07-04 00:07:36 +0200

    fixed drones moving in cutscenes

--- source/character/ai/drone/states/Idle.gd
+++ source/character/ai/drone/states/Idle.gd
@@ -5,25 +5,28 @@ var direction := 1
 export var max_speed := 160
 export var acceleration := 8
 
-onready var timer := $Timer as Timer
+onready var shoot_timer := $Timer as Timer
 onready var flip_timer := $FlipTimer as Timer
 
 func enter(host: Node) -> void:
 	host = host as Drone
 	host.play_lower("hover")
-	timer.start()
+	shoot_timer.start()
 
 func update(host: Node, delta: float) -> void:
 	host = host as Drone
 
 	if not host.can_move or host.disabled:
 		direction = 0
+		host.motion = Vector2(0, 0)
 	else:
 		direction = -1 if host.is_flipped() else 1
 
-	if direction == 1 and host.terrain_on("right") and not host.disabled:
+	print(direction)
+
+	if direction == 1 and host.terrain_on("right"):
 		direction = -1
-	elif direction == -1 and host.terrain_on("left") and not host.disabled:
+	elif direction == -1 and host.terrain_on("left"):
 		direction = 1
 
 	if host.is_too_far_from_origin() and flip_timer.is_stopped():
@@ -33,12 +36,12 @@ func update(host: Node, delta: float) -> void:
 	if direction == -1:
 		host.motion.x = clamp(host.motion.x - acceleration, -max_speed, max_speed)
 		host.flip_left()
-	else:
+	elif direction == 1:
 		host.motion.x = clamp(host.motion.x + acceleration, -max_speed, max_speed)
 		host.flip_right()
 
 	host.move_and_slide(host.motion, Global.UP)
 
-	if host.is_player_in_shoot_range() and timer.is_stopped() and not host.disabled and host.can_move:
+	if host.is_player_in_shoot_range() and shoot_timer.is_stopped() and not host.disabled and host.can_move:
 		host.shoot()
-		timer.start()
+		shoot_timer.start()
