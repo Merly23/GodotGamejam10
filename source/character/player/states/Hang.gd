47137ca49b9dfd9dad68bf3bc38b8a0351242baf
Aaron Winter <winter.aaron93@gmail.com>
2019-07-17 01:45:44 +0200

    more improvements for the hang state

--- source/character/player/states/Hang.gd
+++ source/character/player/states/Hang.gd
@@ -1,5 +1,9 @@
 extends State
 
+var time := 0.0
+
+export var fall_delay := 0.2
+
 func enter(host: Node) -> void:
 	host = host as Player
 	host.motion.x = 0
@@ -15,11 +19,19 @@ func input(host: Node, event: InputEvent) -> void:
 
 	elif event.is_action_pressed("ui_down"):
 		host.cliff_timer.start()
-		host.fsm.change_state("fall")
+		host.fsm.change_state("slide")
 
-	elif host.is_turning_on_wall():
-		host.cliff_timer.start()
-		host.fsm.change_state("fall")
+func update(host: Node, delta: float) -> void:
+	host = host as Player
+	
+	if host.is_turning_on_wall():
+		time += delta
+		if time > fall_delay:
+			host.flip()
+			host.fsm.change_state("fall")
+			host.cliff_timer.start()
+	else:
+		time = 0.0
 
 func exit(host: Node) -> void:
 	host = host as Player
