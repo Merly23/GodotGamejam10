df6abbf7972c720dc5819ccebe0dbcf5cc8c28a9
Aaron Winter <winter.aaron93@gmail.com>
2019-07-18 00:47:26 +0200

    turn before attack

--- source/character/ai/boss/clasher/states/Bite.gd
+++ source/character/ai/boss/clasher/states/Bite.gd
@@ -7,6 +7,14 @@ var time := 0.0
 func enter(host: Node) -> void:
 	host = host as Clasher
 	time = 0.0
+
+	var direction = host.get_player_direction()
+
+	if direction == 1:
+		host.flip_right()
+	elif direction == -1:
+		host.flip_left()
+
 	host.anim_player.play("attack")
 
 func update(host: Node, delta: float) -> void:
