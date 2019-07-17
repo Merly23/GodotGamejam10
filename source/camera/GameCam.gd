f0ca613168c271712656cd4b903a3576a8f75a71
Aaron Winter <winter.aaron93@gmail.com>
2019-07-17 21:56:07 +0200

    slightly slower cam shake

--- source/camera/GameCam.gd
+++ source/camera/GameCam.gd
@@ -16,8 +16,8 @@ func _process(delta: float) -> void:
 
 func shake(amplitude: int) -> void:
 	var new_offset = Vector2(rand_range(-amplitude, amplitude), rand_range(-amplitude, amplitude))
-	tween.interpolate_property(self, "offset", offset, new_offset, 0.1, Tween.TRANS_SINE, Tween.EASE_IN)
-	tween.interpolate_property(self, "offset", offset, Vector2(0, 0), 0.1, Tween.TRANS_SINE, Tween.EASE_OUT, 0.1)
+	tween.interpolate_property(self, "offset", offset, new_offset, 0.2, Tween.TRANS_SINE, Tween.EASE_IN)
+	tween.interpolate_property(self, "offset", offset, Vector2(0, 0), 0.2, Tween.TRANS_SINE, Tween.EASE_OUT, 0.2)
 	tween.start()
 
 func change_target(new_target: Node2D, wait_time: float = 0) -> void:
